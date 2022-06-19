99eb75dfd14c63f4253e581c09727fcb03588def
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2022-06-18 22:33:43 -0300

    API response

---
+++ src/tmdb.js
@@ -0,0 +1,83 @@
+const API_KEY = "a01ddb3b571669308a2126341b2d691e";
+const API_BASE = "https://api.themoviedb.org/3";
+
+/* 
+- originais netflix
+- recomendados (trending)
+- em alta (top rated)
+- ação 
+- comédia
+- terror
+- romance
+- documentários
+*/
+
+const basicFetch = async (endpoint) => {
+  const req = await fetch(`${API_BASE}${endpoint}`);
+  const json = await req.json();
+  return json;
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  getHomeList: async () => {
+    return [
+      {
+        slug: "originals",
+        title: "Originais do Netflix",
+        items: await basicFetch(
+          `/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "trending",
+        title: "Recomendados para você",
+        items: await basicFetch(
+          `/trending/all/week?language=pt-BR&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "toprated",
+        title: "Em Alta",
+        items: await basicFetch(
+          `/movie/top_rated?language=pt-BR&api_key=${API_KEY}`
+        ),
+      },
+      {
+        slug: "action",
+        title: "Ação",
+        items: await basicFetch(
+          `/discover/movie?api_key=${API_KEY}&with_genres=28`
+        ),
+      },
+      {
+        slug: "comedy",
+        title: "Comédia",
+        items: await basicFetch(
+          `/discover/movie?api_key=${API_KEY}&with_genres=35`
+        ),
+      },
+      {
+        slug: "horror",
+        title: "Terror",
+        items: await basicFetch(
+          `/discover/movie?api_key=${API_KEY}&with_genres=27`
+        ),
+      },
+      {
+        slug: "romance",
+        title: "Romance",
+        items: await basicFetch(
+          `/discover/movie?api_key=${API_KEY}&with_genres=10749`
+        ),
+      },
+      {
+        slug: "documentary",
+        title: "Documentários",
+        items: await basicFetch(
+          `/discover/movie?api_key=${API_KEY}&with_genres=99`
+        ),
+      },
+    ];
+  },
+};
